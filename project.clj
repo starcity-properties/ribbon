d34847bb25baef22013e5c076d5a7e795fc71194
Josh Lehman <jalehman37@gmail.com>
2018-01-05 16:22:04 -0800

    Version 0.8.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/ribbon "0.7.0"
+(defproject starcity/ribbon "0.8.0-SNAPSHOT"
   :description "core.async based interface to Stripe REST API."
   :url "https://github.com/starcity-properties/ribbon"
   :license {:name "Eclipse Public License"
